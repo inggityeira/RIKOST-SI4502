39d7f88ee87ec6ca9887da5f79307672668b34eb
inggityeira <inggitagranata22@gmail.com>
2023-12-21 19:03:01 +0700

    edit layout

--- RIKOST/resources/views/kebersihan/index.blade.php
+++ RIKOST/resources/views/kebersihan/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.main')
 
+@section('title', '..........')
+<!-- di titik-titik masukin title contoh: RIKOST | Gedung-->
+
+@section('css')
+<!-- masukin link file css disini contoh: <link rel="stylesheet" href="css/home.css"> -->
+
 @section('content')
 <!-- masukin isi content disini -->
 
