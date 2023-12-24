1fef73a320b0fc6dd65df5139867eea3a639ffa5
Lauratsanaullailla <116336513+Lauratsanaullailla@users.noreply.github.com>
2023-12-24 19:27:43 +0700

    Revert "nikita"

---
+++ RIKOST/resources/views/home.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RIKOST | HOME</title>
+    <link rel="stylesheet" href="css/home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/"><h2><strong>RIKOST</strong></h2></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto me-5" style="font-size: 18px;">
+                <a class="nav-link active me-4" aria-current="page" href="/">Home</a>
+                <a class="nav-link active me-4" aria-current="page" href="/penyewa">Penyewa</a>
+                <a class="nav-link active me-4" aria-current="page" href="/pembayaran">Pembayaran</a>
+                <a class="nav-link active me-4" aria-current="page" href="/laundry">Laundry</a>
+                <a class="nav-link active me-4" aria-current="page" href="/kebersihan">Kebersihan</a>
+                <a class="nav-link active me-4" aria-current="page" href="/pegawai">Pegawai</a>
+                <a class="btn btn-outline-dark" style="background-color:#C8A66B;" aria-current="page" href="/kamar">Kamar</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+    <div class="row">
+        <div class="col">
+            <section id='home'>
+                <p>PILIHAN TERBAIK ANAK KOST!</p>
+                <h1 >Kost ini Lebih dari<br> Sekedar Tempat<br> Tinggal</h1>
+            </section>
+        </div>
+        <div class="col">
+            <img src="img/home.png" alt="home" width="550px" id="icon">
+        </div>
+    </div>
+    </div>
+    
+</body>
+</html>
---
+++ RIKOST/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,21 @@
+<!-- Nav -->
+<nav class="navbar navbar-expand-lg" style="background-color: #CBA96D;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/"><h2><strong>RIKOST</strong></h2></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto me-5" style="font-size: 18px;">
+            <a class="nav-link active me-4" aria-current="page" href="/">Home</a>
+            <a class="nav-link active me-4" aria-current="page" href="/penyewa">Penyewa</a>
+            <a class="nav-link active me-4" aria-current="page" href="/pembayaran">Pembayaran</a>
+            <a class="nav-link active me-4" aria-current="page" href="/penyewa">Laundry</a>
+            <a class="nav-link active me-4" aria-current="page" href="/kebersihan">Kebersihan</a>
+            <a class="nav-link active me-4" aria-current="page" href="/pegawai">Pegawai</a>
+            <a class="nav-link active me-4" aria-current="page" href="/kamar">kamar</a>
+        </div>
+        </div>
+    </div>
+</nav>
+<!-- Nav End -->
