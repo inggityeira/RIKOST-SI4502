bbe6ac2a474f7c509acba912ade08653f7988818
Lauratsanaullailla <tsnalauralailla@gmail.com>
2023-12-25 06:17:53 +0700

    landing

---
+++ RIKOST/resources/views/landing/landing.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RIKOST | LANDING</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/landing.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent" style ="margin-left: 60px; margin-top:40px; margin-right: 30px;">
+        <div class="container-fluid">
+        <h2><strong>RIKOST</strong></h2> 
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto me-5" style="font-size: 18px;">
+                <a class="nav-link active me-4" aria-current="page" href="/ourteam">Our Team</a>
+                <a class="nav-link active me-4" style="color:#A38859;" aria-current="page" href="/login">Login</a>
+                <a class="btn" style="background-color:#A38859; color:white; font-weight: 600; " aria-current="page" href="/signup">Sign Up</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="center-right-container">
+    <div class="row">
+        <div class="col">
+            <section id='landpage'>
+                <p>WELCOME!</p>
+                <h1>let’s improve our boarding house services!<br>A well-managed place is a home away from home.<br> Let’s strive for excellence together!</h1>
+            </section>
+        </div>
+        <div class="col">
+            <img src="img/landing.png" alt="landing" id="foto">
+        </div>
+    </div>
+    </div>
+</body>
+</html>
