99be6263357d0c34316788d592d6184a07ad8065
inggityeira <inggitagranata22@gmail.com>
2023-12-28 18:47:43 +0700

    Laundry hubung dengan pegawai

---
+++ RIKOST/app/Models/laundry.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class laundry extends Model
+{
+    use HasFactory;
+
+    protected $table = 'laundry';
+
+    protected $fillable = [
+        'jenis_laundry', 'id_penyewa', 'berat_laundry', 'tagihan_laundry', 'tgl_pemesanan', 'tgl_selesai', 'status_laundry', 'id_pegawai'
+    ];
+
+    // public function penyewa(){
+    //     return $this->belongsTo(penyewa::class);
+    // }
+
+    public function pegawai(){
+        return $this->belongsTo('App\pegawai');
+    }
+}
