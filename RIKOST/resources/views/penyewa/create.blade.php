1b83d8e2c99a7366dec23ffc0e0c5bd75304ec21
Lauratsanaullailla <tsnalauralailla@gmail.com>
2023-12-29 16:24:56 +0700

    penyewaCU

--- RIKOST/resources/views/penyewa/create.blade.php
+++ RIKOST/resources/views/penyewa/create.blade.php
@@ -3,10 +3,6 @@
 @section('title', 'RIKOST | Laundry')
 <!-- di titik-titik masukin title contoh: RIKOST | Gedung-->
 
-@push('css')
-    <!-- masukin <link> yang nyambungin ke file css -->
-@endpush
-
 @section('content')
 <!-- masukin isi content disini -->
 <div class = "card" style = "margin : 20px;">
@@ -14,33 +10,31 @@
     <div class = "card-body">
         <form>
         <label>Masukan Id</label></br>
-        <input type="integer" name="id" id = "id" class="form-control"></br>
+        <input type="integer" name="id_penyewa" id = "id_penyewa" class="form-control"></br>
 
-        <label>Nama</label></br>
-        <input type="string" name="nama" id = "nama" class="form-control"></br>
+        <label>Nama Penyewa</label></br>
+        <input type="string" name="nama_penyewa" id = "nama_penyewa" class="form-control"></br>
 
-        <label>Kontak</label></br>
-        <input type="string" name="kontak" id = "kontak" class="form-control"></br>
+        <label>Kontak Penyewa</label></br>
+        <input type="string" name="kontak_penyewa" id = "kontak_penyewa" class="form-control"></br>
 
-        <label>NIK</label></br>
-        <input type="string" name="nik" id = "nik" class="form-control"></br>
+        <label>NIK Penyewa</label></br>
+        <input type="string" name="NIK_penyewa" id = "NIK_penyewa" class="form-control"></br>
         
         <label>Nama Orang Tua</label></br>
-        <input type="string" name="ortu" id = "ortu" class="form-control"></br>
-        
-        <label>Kontak</label></br>
-        <input type="password" name="kontak" id = "kontak" class="form-control"></br>
+        <input type="string" name="nama_ortu" id = "nama_ortu" class="form-control"></br>
         
         <label>Kontak Orang Tua</label></br>
-        <input type="string" name="kontakortu" id = "kontakortu" class="form-control"></br>
+        <input type="string" name="kontak_ortu" id = "kontak_ortu" class="form-control"></br>
 
         <label>Waktu Sewa</label></br>
-        <input type="string" name="waktusewa" id = "waktusewa" class="form-control"></br>
+        <input type="string" name="waktu_sewa" id = "waktu_sewa" class="form-control"></br>
 
-        <!-- masukin FK dari Kamar -->
+        <label>ID Kamar</label></br>
+        <input type="string" name="id_kamar" id = "id_kamar" class="form-control"></br>
         
         <label>Fasilitas Umum</label></br>
-        <input type="string" name="fasilitas" id = "fasilitas" class="form-control"></br>
+        <input type="string" name="fasilitas_umum" id = "fasilitas_umum" class="form-control"></br>
         
         <input type="submit" value= "Submit" class="btn" style = "background-color: #A38859 ; color : #FFFFFF "></br>
         </form>
