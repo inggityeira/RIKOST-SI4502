cdb6c3195f5a82d6cda9cfaa173a67007b8974b2
Lauratsanaullailla <tsnalauralailla@gmail.com>
2023-12-29 16:52:43 +0700

    penyewaController

---
+++ RIKOST/app/Http/Controllers/penyewaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\penyewa;
+use App\Models\laundry;
+use App\Models\kamar;
+
+class penyewaController extends Controller
+{
+    //
+}
