945b3619117c97dcd73b366d2f361806c0a97f7a
Lauratsanaullailla <tsnalauralailla@gmail.com>
2023-12-31 03:57:50 +0700

    oureteam

--- RIKOST/routes/web.php
+++ RIKOST/routes/web.php
@@ -33,10 +33,11 @@ Route::post('/login-user', [AuthManager::class, 'loginUser'])->name('login-user'
 Route::get('/home', [AuthManager::class, 'home'])->middleware('isLoggedIn');
 Route::get('/logout', [AuthManager::class, 'logout']);
 Route::get('/', [halamanController::class, 'landing'])->middleware('alreadyLoggedIn');
+Route::get('/ourteam', [halamanController::class, 'ourteam'])->middleware('alreadyLoggedIn');
 
-Route::get('/ourteam', function () {
-    return view('landing.ourteam');
-});
+// Route::get('/ourteam', function () {
+//     return view('landing.ourteam');
+// });
 // inggit
 Route::get('/laundry', [LaundryController::class, 'informasi'])->middleware('isLoggedIn')->name('laundry.list');
 Route::get('/newlaundry', [LaundryController::class, 'newlaundry'])->middleware('isLoggedIn')->name('laundry.new');
