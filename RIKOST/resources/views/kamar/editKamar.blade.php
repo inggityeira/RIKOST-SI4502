548c07dac634326333071a9d35ce0362781dfe2d
Rico Galuh <ricopamungkas20@gmail.com>
2023-12-30 18:13:55 +0700

    Edit Kamar, dan Web

---
+++ RIKOST/resources/views/kamar/editKamar.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.main')
+
+@section('title', 'RIKOST | TAMBAH KAMAR')
+<!-- di titik-titik masukin title contoh: RIKOST | Gedung-->
+
+@section('content')
+<!-- masukin isi content disini -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<main class="mt-5 pt-3">
+    <div class="container-fluid">
+        <h1 class="container m-5">Edit Kamar</h1>
+        <div class="row container m-5">
+        <form action="{{  route('kamar.update', $idkamar) }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="col-12">
+                            <label for="inputNanme4" class="form-label">Lantai Kamar</label>
+                            <input type="text" class="form-control @error('lantai_kamar') is-invalid @enderror" id="lantai_kamar" name="lantai_kamar" value="{{ old('lantai_kamar',$data->lantai_kamar) }}" placeholder="Lantai Kamar">
+                            @if ($errors->has('lantai_kamar'))
+                            <span class="text-danger">{{ $errors->first('lantai_kamar') }}</span>
+                            @endif
+                        </div>
+                        <br>
+                        <div class="col-12">
+                            <label for="inputNanme4" class="form-label">Jenis Kamar</label>
+                            <select class="form-control @error('jenis_kamar') is-invalid @enderror" id="jenis_kamar" name="jenis_kamar" value="{{ old('jenis_kamar',$data->jenis_kamar) }}">
+                                <option value="{{$data->jenis_kamar}}">{{$data->jenis_kamar}}</option>
+                                <option value="">-- Pilih Jenis Kamar --</option>
+                                <option value="Suite">Suite</option>
+                                <option value="Medium">Medium</option>
+                                <option value="Reguler">Reguler</option>
+                            </select>
+                            @if ($errors->has('jenis_kamar'))
+                            <span class="text-danger">{{ $errors->first('jenis_kamar') }}</span>
+                            @endif
+                        </div>
+                        <br>
+                        <div class="col-12">
+                                <label class="font-weight-bold">Foto Kamar</label>
+                                <input type="file" class="form-control @error('foto_kamar') is-invalid @enderror" name="foto_kamar">
+                                @error('foto_kamar')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                        </div>
+                        <br>
+                        <div class="col-12">
+                            <label for="inputNanme4" class="form-label">Fasilitas Kamar</label>
+                            <textarea class="form-control @error('fasilitas_kamar') is-invalid @enderror" id="fasilitas_kamar" name="fasilitas_kamar" value="" cols="30" rows="10" placeholder="Fasiltas Kamar">{{ old('fasilitas_kamar',$data->fasilitas_kamar) }}</textarea>
+                            @if ($errors->has('fasilitas_kamar'))
+                            <span class="text-danger">{{ $errors->first('fasilitas_kamar') }}</span>
+                            @endif
+                        </div>
+                        <br>
+                        <div class="col-12">
+                            <label for="inputNanme4" class="form-label">Harga Kamar</label>
+                            <input type="number" class="form-control @error('harga_kamar') is-invalid @enderror" id="harga_kamar" name="harga_kamar" value="{{ old('harga_kamar',$data->harga_kamar) }}" placeholder="Harga Kamar">
+                            @if ($errors->has('harga_kamar'))
+                            <span class="text-danger">{{ $errors->first('harga_kamar') }}</span>
+                            @endif
+                        </div>
+                        <br>
+                        <div class="col-12">
+                            <label for="inputNanme4" class="form-label">Status Kamar</label>
+                            <input type="text" class="form-control @error('status_kamar') is-invalid @enderror" id="status_kamar" name="status_kamar" value="{{ old('status_kamar',$data->status_kamar) }}" placeholder="Status Kamar">
+                            @if ($errors->has('status_kamar'))
+                            <span class="text-danger">{{ $errors->first('status_kamar') }}</span>
+                            @endif
+                        </div>
+                        <div class="col-12">
+                            <br>
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                            <button type="reset" class="btn btn-secondary">Reset</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+            <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+            <script>
+                CKEDITOR.replace('content');
+            </script>
+        </div>
+</main>
+</body>
+
+</html>
+
+@endsection
--- RIKOST/resources/views/kamar/identitaskamar.blade.php
+++ RIKOST/resources/views/kamar/identitaskamar.blade.php
@@ -3,44 +3,11 @@
 @section('title', 'RIKOST | IDENTITAS KAMAR')
 <!-- di titik-titik masukin title contoh: RIKOST | Gedung-->
 
+@push('css')
+    <!-- masukin <link> yang nyambungin ke file css -->
+@endpush
+
 @section('content')
-<!-- masukin isi content disini -->
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Suite Class</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Connection
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -56,11 +23,11 @@
             
             <div class="col-md-8">
 
-                <h1>Suite Class Bedroom</h1>
+                <h1>{{ $data->jenis_kamar }}</h1>
                 <p>Alamat: Jalan Gatot Subroto no.24</p>
                 <p>Email: RIKOST@gmail.com</p>
                 <p>Telepon: +6281234567</p>
-                <p>Status: <a href="#" class="btn btn-primary mr-2">Available</a></p>
+                <p>Status: <a href="#" class="btn btn-primary mr-2">{{ $data->status_kamar }}</a></p>
 
                 <div class="mt-3">
                     <a href="#" class="btn btn-primary mr-2">Facebook</a>
@@ -72,7 +39,11 @@
             </div><div class="col-md-4">
 
                 <a href="https://yt3.googleusercontent.com/1NIzmAWHaYMjO_MgOT7DrhvjiEneGmcgb4QXVZbh53dxJ2A8OmBaTbgSzRBrtHLucjLF8KSZRCI=s900-c-k-c0x00ffffff-no-rj" target="_blank">
-                    <img src="img/Kamar Suite class.jpeg" alt="Foto Profil" class="img-fluid rounded-circle">
+                  @if(!$data->foto_kamar)
+                    <img src="{{ asset('img/kamar.jpg')}}" alt="" class="img-fluid rounded-circle">
+                    @else
+                    <img src="{{ asset('storage/uploads/'.$data->foto_kamar)}}" class="img-fluid rounded-circle" alt="Foto Tidak Tersedia">
+                    @endif  
                 </a>
             </div>
             
@@ -100,16 +71,7 @@
             <ul class="list-unstyled">
                 <li>Fasilitas yang tercantum dapat digunakan oleh penghuni kost</li>
                 <li>
-                  <ul>
-                    <li>Air Conditioner</li>
-                    <li>Tempat Tidur</li>
-                    <li>Lemari pakaian</li>
-                    <li>Meja Belajar</li>
-                    <li>Lampu Tidur</li>
-                    <li>Kursi Belajar</li>
-                    <li>Kamar mandi dalam</li>
-                    <li>Water heater</li>
-                  </ul>
+                  {{ $data->fasililtas_kamar }}
                 </li>
               </ul>
           </figure>
@@ -117,52 +79,9 @@
         <h1 class="display-6">Harga</h1>
         <figure>
             <blockquote class="blockquote">
-              <p>Harga</p>
+              <p>{{ number_format($data->harga_kamar, 2, '.', ',')}}</p>
             </blockquote>
-            <figcaption class="blockquote-footer">
-                text
-            </figcaption>
-            <ul class="list-unstyled">
-                <li>text</li>
-                <li>
-                  <ul>
-                    <li>text</li>
-                  </ul>
-                </li>
-              </ul>
           </figure>
-          <hr style="border-width: 5px;">
-          <h1 class="display-6">text</h1>
-          <figure>
-              <blockquote class="blockquote">
-                <p>text</p>
-              </blockquote>
-              <figcaption class="blockquote-footer">
-                  text
-              </figcaption>
-              <ul class="list-unstyled">
-                  <li>text</li>
-                  <li>
-                    <ul>
-                      <li>text</li>
-                      <li>text</li>
-                    </ul>
-                  </li>
-                </ul>
-            </figure>
-            <hr style="border-width: 5px;">
-          <h1 class="display-6">text</h1>
-            <figure>
-              <ul class="list-unstyled">
-                  <li>
-                    <ul>
-                      <li>text</li>
-                      <li>textt</li>
-                      <li>textt</li>
-                    </ul>
-                  </li>
-                </ul>
-            </figure>
     </div>
     <div>
     </div>
@@ -174,4 +93,3 @@
 
 
 @endsection
-
