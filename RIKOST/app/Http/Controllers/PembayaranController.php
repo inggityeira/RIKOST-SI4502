e82ce11fbcb7b961c024e1ad818ce58b9f63593e
Nikitags180 <nikitae372@gmail.com>
2023-12-30 15:43:41 +0700

    ViewEdit

--- RIKOST/app/Http/Controllers/PembayaranController.php
+++ RIKOST/app/Http/Controllers/PembayaranController.php
@@ -32,25 +32,5 @@ class PembayaranController extends Controller
         return redirect()->route('home.pembayaran')->with('success','pembayaran berhasil ditambahkan');
 
     }
-    public function edit($id_pembayaran){
-        $list = Pembayaran::where('id_pembayaran', '=', $id_pembayaran)->get();
-        return view('pembayaran.editPembayaran', compact('list'));
-    }
-    public function update(Request $request, $id_pembayaran){
-        Pembayaran::where('id_pembayaran', '=', $id_pembayaran)->update([
-            'id_penyewa' => $request->id_penyewa,
-            'tgl_pembayaran' => $request->tgl_pembayaran,
-            'termin_pembayaran' => $request->termin_pembayaran,
-            'nominal_pembayaran' => $request->nominal_pembayaran,
-            'aspek_pembayaran' => $request->aspek_pembayaran,
-            'metode_pembayaran' => $request->metode_pembayaran,
-            'status_pembayaran' => $request->status_pembayaran,
-        ]);
-        return redirect('/pembayaran');
-    }
-    public function destroy($id_pembayaran){
-        Pembayaran::where('id_pembayaran', '=', $id_pembayaran)->delete();
-        return redirect('/pembayaran');
-    }
 }
 
