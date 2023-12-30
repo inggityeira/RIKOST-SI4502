fe76bd63c78cd5bbb945ec7305e3aa27a7d34cd4
Nikitags180 <nikitae372@gmail.com>
2023-12-30 10:45:16 +0700

    CreatePembayaran

--- RIKOST/app/Http/Controllers/PembayaranController.php
+++ RIKOST/app/Http/Controllers/PembayaranController.php
@@ -2,27 +2,35 @@
 
 namespace App\Http\Controllers;
 use App\Models\Pembayaran;
-use App\Models\Penyewa;
+use App\Models\penyewa;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class PembayaranController extends Controller
 {
     public function index(){
-        $list = Pembayaran::all();
+        $list = Pembayaran::with('penyewa')->get();
         return view('pembayaran.listPembayaran', compact('list'));
     }
     public function store(Request $request){
-        Pembayaran::create([
+        $tanggalFormatBaru = Carbon::createFromFormat('Y-m-d', $request->tgl_pembayaran)->format('Y-m-d');
+        // dd($tanggalFormatBaru);
+        $pembayaran =  Pembayaran::create([
             'id_penyewa' => $request->id_penyewa,
-            'tgl_pembayaran' => $request->tgl_pembayaran,
+            'tgl_pembayaran' => $tanggalFormatBaru,
             'termin_pembayaran' => $request->termin_pembayaran,
             'nominal_pembayaran' => $request->nominal_pembayaran, 
             'aspek_pembayaran' => $request->aspek_pembayaran,
             'metode_pembayaran' => $request->metode_pembayaran,
             'status_pembayaran' => $request->status_pembayaran,
         ]);
-        return redirect('/pembayaran');
+        // return redirect('/pembayaran');
+        
+        // $pembayaran = Pembayaran::create($request->all());
+
+        return redirect()->route('home.pembayaran')->with('success','pembayaran berhasil ditambahkan');
+
     }
     public function edit($id_pembayaran){
         $list = Pembayaran::where('id_pembayaran', '=', $id_pembayaran)->get();
--- RIKOST/app/Models/Pembayaran.php
+++ RIKOST/app/Models/Pembayaran.php
@@ -4,15 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\penyewa;
 
 class Pembayaran extends Model
 {
     use HasFactory;
-    protected $table = 'pembayaran';
-    protected $primary_key = 'id_pembayaran';
-    protected $guarded = ['id_pembayaran'];
+
+    protected $table = "pembayaran";
+
+    protected $fillable = [
+        'id_pembayaran', 'id_penyewa', 'tgl_pembayaran', 'termin_pembayaran', 'nominal_pembayaran', 'aspek_pembayaran', 'metode_pembayaran', 'status_pembayaran'
+    ];
+    
     public function penyewa()
     {
-        return $this->belongsTo(Penyewa::class);
-    }
+        return $this->belongsTo(penyewa::class, 'id_penyewa', 'id_penyewa');
+}
+    
 }
