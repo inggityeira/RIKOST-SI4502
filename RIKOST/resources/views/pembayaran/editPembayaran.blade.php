b7066a297da1e070381a06bcb0362856556dabc8
Nikitags180 <nikitae372@gmail.com>
2023-12-31 00:23:49 +0700

    TabelPembayaran

--- RIKOST/resources/views/pembayaran/editPembayaran.blade.php
+++ RIKOST/resources/views/pembayaran/editPembayaran.blade.php
@@ -7,6 +7,7 @@
 <!-- masukin <link> yang nyambungin ke file css -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 @section('content')
 <!-- masukin isi content disini -->
 <head>
@@ -22,51 +23,67 @@
             <form method="POST" action="{{route('update.pembayaran', $list->id_pembayaran)}}">
                 @csrf
                 @method('PUT')
-      
+
               <!-- Tanggal Pembayaran -->
               <div class="mb-4">
                 <label for="tgl_pembayaran" class="form-label">Tanggal Pembayaran</label>
                 <input type="date" name="tgl_pembayaran" id="tgl_pembayaran" class="form-control" placeholder="Tanggal Pembayaran"  value="{{$list->id_pembayaran}}" />
               </div>
-      
+
               <!-- Termin Pembayaran -->
               <div class="mb-4">
                 <label for="termin_pembayaran" class="form-label">Termin Pembayaran</label>
-                <input type="text" name="termin_pembayaran" id="termin_pembayaran" class="form-control" placeholder="Termin Pembayaran" value="{{$list->termin_pembayaran}}" />
-              </div>
-      
+                <select name="termin_pembayaran" id="termin_pembayaran" class="form-select">
+              <option value="Satu">1</option>
+              <option value="Dua">2</option>
+              <!-- Tambahkan opsi lain sesuai kebutuhan Anda -->
+            </select>
+          </div>
+
               <!-- Nominal Pembayaran -->
               <div class="mb-4">
                 <label for="nominal_pembayaran" class="form-label">Nominal Pembayaran</label>
                 <input type="text" name="nominal_pembayaran" id="nominal_pembayaran" class="form-control" placeholder="Nominal Pembayaran" value="{{$list->nominal_pembayaran}}" />
               </div>
-      
+
               <!-- Aspek Pembayaran -->
               <div class="mb-4">
                 <label for="aspek_pembayaran" class="form-label">Aspek Pembayaran</label>
-                <input type="text" name="aspek_pembayaran" id="aspek_pembayaran" class="form-control" placeholder="Aspek Pembayaran" value="{{$list->aspek_pembayaran}}" />
-              </div>
-      
+                <select name="aspek_pembayaran" id="aspek_pembayaran" class="form-select">
+              <option value="Kosan Tipe Kamar Suite">Kosan Tipe Kamar Suite</option>
+              <option value="Kosan Tipe Kamar Medium">Kosan Tipe Kamar Medium</option>
+              <option value="Kosan Tipe Kamar Reguler">Kosan Tipe Kamar Reguler</option>
+              <option value="Laundry">Laundry</option>
+              <option value="Kebersihan">Kebersihan</option>
+              <!-- Tambahkan opsi lain sesuai kebutuhan Anda -->
+            </select>
+          </div>
+
               <!-- Metode Pembayaran -->
               <div class="mb-4">
                 <label for="metode_pembayaran" class="form-label">Metode Pembayaran</label>
                 <input type="text" name="metode_pembayaran" id="metode_pembayaran" class="form-control" placeholder="Metode Pembayaran" value="{{$list->metode_pembayaran}}" />
               </div>
-      
+
               <!-- Status Pembayaran -->
               <div class="mb-4">
                 <label for="status_pembayaran" class="form-label">Status Pembayaran</label>
-                <input type="text" name="status_pembayaran" id="status_pembayaran" class="form-control" placeholder="Status Pembayaran" value="{{$list->status_pembayaran}}" />
-              </div>
-      
+                <select name="status_pembayaran" id="status_pembayaran" class="form-select">
+              <option value="Belum Lunas">Belum Lunas</option>
+              <option value="Lunas">Lunas</option>
+              <option value="Dibatalkan">Dibatalkan</option>
+              <!-- Tambahkan opsi lain sesuai kebutuhan Anda -->
+            </select>
+          </div>
+
               <!-- Submit button -->
             <button type="submit" class="btn btn-primary btn-block mb-4" style = "background-color: green ; color : white">Save</button>
             <button type="submit" class="btn btn-primary btn-block mb-4" style = "background-color: red ; color : white">Cancel</button>
             </form>
           </div>
         </div>
-      </div>    
-    </div>  
+      </div>
+    </div>
 
 </body>
 </html>
--- RIKOST/resources/views/pembayaran/listPembayaran.blade.php
+++ RIKOST/resources/views/pembayaran/listPembayaran.blade.php
@@ -7,6 +7,12 @@
 <!-- masukin <link> yang nyambungin ke file css -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<style>
+    td, tr {
+        text-align: center
+    }
+</style>
+
 @section('content')
 <!-- masukin isi content disini -->
 <head>
@@ -25,9 +31,6 @@
         <th>ID Pembayaran</th>
         <th>Nama Penyewa</th>
         <th>Tanggal Pembayaran</th>
-        {{-- <th>Termin Pembayaran</th>
-        <th>Nominal Pembayaran</th>
-        <th>Metode Pembayaran</th> --}}
         <th>Aspek Pembayaran</th>
         <th>Status Pembayaran</th>
         <th>Action</th>
@@ -39,9 +42,6 @@
                 <td>{{ $item->id_pembayaran }}</td>
                 <td>{{ $item->penyewa->nama_penyewa}}</td>
                 <td>{{ $item->tgl_pembayaran }}</td>
-                {{-- <td>{{ $item->termin_pembayaran }}</td>
-                <td>{{ $item->nominal_pembayaran }}</td>
-                <td>{{ $item->metode_pembayaran }}</td> --}}
                 <td>{{ $item->aspek_pembayaran }}</td>
                 <td>{{ $item->status_pembayaran }}</td>
                 <td>
@@ -56,7 +56,7 @@
                   <a href="/pembayaran/destroy/{{ $item->id_pembayaran }}" class="">
                     <svg viewBox="0 0 24 24" fill="none" width="28" heigth="28"  xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 7H20" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10L7.70141 19.3578C7.87432 20.3088 8.70258 21 9.66915 21H14.3308C15.2974 21 16.1257 20.3087 16.2986 19.3578L18 10" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke="#ff0000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                   </a>
-                </td>                
+                </td>
             </tr>
         @endforeach
     </tbody>
@@ -117,7 +117,7 @@
               <!-- Tambahkan opsi lain sesuai kebutuhan Anda -->
             </select>
           </div>
-          
+
           <!-- Metode Pembayaran -->
           <div class="mb-3">
             <label for="metode_pembayaran" class="form-label">Metode Pembayaran</label>
@@ -134,7 +134,7 @@
               <!-- Tambahkan opsi lain sesuai kebutuhan Anda -->
             </select>
           </div>
-          
+
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style = "background-color: red ; color : white">Close</button>
