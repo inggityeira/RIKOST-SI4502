8ef4d2705d16180d9397600328c65f3ff0890ab4
lanangarf <ariefqyan@student.telkomuniversity.ac.id>
2023-12-31 11:24:41 +0700

    update hias2 cieses butstrep

--- RIKOST/resources/views/tamu/createTamu.blade.php
+++ RIKOST/resources/views/tamu/createTamu.blade.php
@@ -4,91 +4,90 @@
 <!-- di titik-titik masukin title contoh: RIKOST | Gedung-->
 
 @push('css')
-<!-- masukin <link> yang nyambungin ke file css -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 @endpush
 
 @section('content')
 <!-- masukin isi content disini -->
-<div class="card">
-    <div class="card-header">
-        <h2 class="mb-0">Tambah Tamu Baru</h2>
-    </div>
-    <div class="card-body col-lg-4 ">
-        @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
+<div class="container">
+    <div class="card mt-4 mx-auto">
+        <div class="card-header bg-primary text-white">
+            <h2 class="mb-0">Tambah Tamu Baru</h2>
+        </div>
+        <div class="card-body col-lg-8 mx-auto">
+            @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
 
-        <form action="{{ route('tamu.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
+            <form action="{{ route('tamu.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
 
+                <div class="mb-3">
+                    <label for="idtamu" class="form-label">Nomor Tamu</label>
+                    <input type="text" class="form-control" name="idtamu" required placeholder="Masukkan Nomor Tamu">
+                </div>
 
-            @csrf
+                <div class="mb-3">
+                    <label for="namatamu" class="form-label">Nama</label>
+                    <input type="text" class="form-control" name="namatamu" required placeholder="Masukkan Nama Tamu">
+                </div>
 
-            <div class="form-group">
-                <label for="idtamu">Nomor Tamu</label>
-                <input type="text" class="form-control mb-3" name="idtamu" required placeholder="Masukkan Nama Tamu">
-            </div>
+                <div class="mb-3">
+                    <label for="nohptamu" class="form-label">Nomor HP</label>
+                    <input type="text" class="form-control" name="nohptamu" required placeholder="Masukkan Nomor HP">
+                </div>
 
-            <div class="form-group">
-                <label for="namatamu">Nama:</label>
-                <input type="text" class="form-control mb-3" name="namatamu" required placeholder="Masukkan Nama Tamu">
-            </div>
+                <div class="mb-3">
+                    <label for="kartu" class="form-label">Kartu Identitas</label>
+                    <input type="text" class="form-control" name="kartu" required placeholder="Kartu Identitas">
+                </div>
 
-            <div class="form-group">
-                <label for="nohptamu">Nomor HP:</label>
-                <input type="text" class="form-control mb-3" name="nohptamu" required placeholder="Masukkan Nomor HP">
-            </div>
+                <div class="mb-3">
+                    <label for="waktu_checkin" class="form-label">Waktu Check-in</label>
+                    <input type="date" class="form-control" name="masuk" required>
+                </div>
 
-            <div class="form-group">
-                <label for="kartu">Kartu Identitas</label>
-                <input type="text" class="form-control mb-3" name="kartu" required placeholder="Kartu Identitas">
-            </div>
+                <div class="mb-3">
+                    <label for="waktu_checkout" class="form-label">Waktu Check-out</label>
+                    <input type="date" class="form-control" name="keluar" required>
+                </div>
 
-            <div class="form-group">
-                <label for="waktu_checkin">Waktu Check-in:</label>
-                <input type="date" class="form-control mb-3" name="masuk" required>
-            </div>
-
-            <div class="form-group">
-                <label for="waktu_checkout">Waktu Check-out:</label>
-                <input type="date" class="form-control mb-3" name="keluar" required>
-            </div>
-
-            <div>
-                @if (count($kamar) > 0)
-                <select class="form-select mb-3" aria-label="Masukkan Data Kamar" name="kamar">
-                    <option selected>Nomor Kamar</option>
-
-                    @foreach ($kamar as $item)
-                    <option value="{{ $item->id_kamar }}">{{ $item->id_kamar }}</option>
-                    @endforeach
-                </select>
-                @else
-                <input type="text" id="disabledTextInput" class="form-control mb-3" placeholder="Belum ada data penyewa"
-                    disabled>
-                @endif
-
-                <div>
-                    @if (count($penyewa) > 0)
-                    <select class="form-select mb-3" aria-label="Masukkan data penyewa" name="penyewa">
-                        <option selected>Nama Penyewa</option>
-
-                        @foreach ($penyewa as $item)
-                        <option value="{{ $item->id_penyewa }}">{{ $item->nama_penyewa }}</option>
+                <div class="mb-3">
+                    @if (count($kamar) > 0)
+                    <label for="kamar" class="form-label" name="kamar"> Nomor Kamar </label>
+                    <select class="form-select" aria-label="Masukkan Data Kamar" name="kamar">
+                        <option selected>Pilih Kamar</option>
+                        @foreach ($kamar as $pilih)
+                        <option value="{{ $pilih->id_kamar }}">{{ $pilih->id_kamar }}</option>
                         @endforeach
                     </select>
                     @else
-                    <input type="text" id="disabledTextInput" class="form-control mb-3"
-                        placeholder="Belum ada data penyewa" disabled>
+                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Belum ada data kamar"
+                        disabled>
                     @endif
-
                 </div>
-                <button type="submit" class="btn btn-primary mb-3">Submit</button>
-        </form>
+
+                <div class="mb-4">
+                    @if (count($penyewa) > 0)
+                    <label for="penyewa" class="form-label" name="penyewa"> Nama Penyewa Penanggung Jawab </label>
+                    <select class="form-select" aria-label="Masukkan data penyewa" name="penyewa">
+                        <option selected>Pilih Nama Penyewa</option>
+                        @foreach ($penyewa as $pilih)
+                        <option value="{{ $pilih->id_penyewa }}">{{ $pilih->nama_penyewa }}</option>
+                        @endforeach
+                    </select>
+                    @else
+                    <input type="text" id="disabledTextInput" class="form-control" placeholder="Belum ada data penyewa"
+                        disabled>
+                    @endif
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
 </div>
 </body>
-
-</html>
 
 @endsection
--- RIKOST/resources/views/tamu/listTamu.blade.php
+++ RIKOST/resources/views/tamu/listTamu.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'RIKOST | Daftar Tamu')
 
 @push('css')
-<!-- Add any additional CSS links if needed -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 @endpush
 
 @section('content')
